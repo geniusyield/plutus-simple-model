e1bde7d67d5f80075d5f3f64b65cd2a1cc1325cd
Valentino Cerutti <valentinocerutti@gmail.com>
2023-08-14 21:05:45 -0300

    WIP Feat #168: removes plutarch references

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   nixConfig = {
     # We don't use Recursive Nix yet.
     extra-experimental-features = [ "nix-command" "flakes" "ca-derivations" "recursive-nix" ];
-    extra-substituters = [ "https://cache.iog.io" "https://public-plutonomicon.cachix.org" "https://mlabs.cachix.org" ];
+    extra-trusted-substituters = [ "https://cache.iog.io" "https://public-plutonomicon.cachix.org" "https://mlabs.cachix.org" ];
     extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "public-plutonomicon.cachix.org-1:3AKJMhCLn32gri1drGuaZmFrmnue+KkKrhhubQk/CWc=" ];
     allow-import-from-derivation = "true";
     bash-prompt = "\\[\\e[0m\\][\\[\\e[0;2m\\]nix \\[\\e[0;1m\\]psm \\[\\e[0;93m\\]\\w\\[\\e[0m\\]]\\[\\e[0m\\]$ \\[\\e[0m\\]";
@@ -11,22 +11,15 @@
 
   inputs = {
     tooling.url = "github:mlabs-haskell/mlabs-tooling.nix";
-    plutarch.url = "github:plutonomicon/plutarch-plutus";
   };
 
-  outputs = inputs@{ self, tooling, plutarch, nixpkgs, ... }:
+  outputs = inputs@{ self, tooling, nixpkgs, ... }:
     tooling.lib.mkFlake { inherit self; }
       {
         imports = [
           (tooling.lib.mkHaskellFlakeModule1 {
             project.src = ./.;
-            project.extraHackage = [
-              "${plutarch}"
-              "${plutarch}/plutarch-extra"
-            ];
             toHaddock = [
-              "plutarch"
-              "plutarch-extra"
               "cardano-crypto"
               "cardano-ledger-alonzo"
               "cardano-ledger-babbage"
